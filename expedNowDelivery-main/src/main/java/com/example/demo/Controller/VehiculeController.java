921f905ad30cacb81dd76790b632072061d41d0a
asma ben khalifa <36479510+asma-ben-khalifa@users.noreply.github.com>
2025-08-10 08:24:26 +0000

    af

--- expedNowDelivery-main/src/main/java/com/example/demo/Controller/VehiculeController.java
+++ expedNowDelivery-main/src/main/java/com/example/demo/Controller/VehiculeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.example.demo.ServiceApplicatif.VehiculeServiceApp;
 import com.example.demo.ModelDTO.SaveVehiculeRequestDTO;
 import com.example.demo.ModelDTO.UpdatevehiculeRequestDTO;;
 
+@CrossOrigin(origins = "*")
 
 @RestController
 @RequestMapping("/vehicules")
